915caeeec7004f440d455cdf14a7efa642e06632
Dmitriy Ivanko <tmwsls12@gmail.com>
2023-07-07 17:34:07 +0300

    Do not use getenv in global_settings.py

--- hammett/conf/global_settings.py
+++ hammett/conf/global_settings.py
@@ -3,12 +3,10 @@ Default Hammett settings. Override these using the module specified via
 the HAMMETT_SETTINGS_MODULE environment variable.
 """
 
-import os
-
 HIDERS_CHECKER = ''
 
 HTML_PARSE_MODE = True
 
 PERMISSIONS = []
 
-TOKEN = os.getenv('TOKEN', '')
+TOKEN = ''
