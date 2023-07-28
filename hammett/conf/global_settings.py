e4d71504787ec2d8172ad075a23e4aea7b740e76
Dmitriy Ivanko <tmwsls12@gmail.com>
2023-07-28 18:43:27 +0300

    Fix PERMISSIONS setting typing

--- hammett/conf/global_settings.py
+++ hammett/conf/global_settings.py
@@ -8,15 +8,13 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing import Any
 
-    from hammett.core.permissions import Permission
-
 HIDERS_CHECKER = ''
 
 HTML_PARSE_MODE = True
 
 LOGGING: dict[str, 'Any'] = {}
 
-PERMISSIONS: list[type['Permission']] = []
+PERMISSIONS: list[str] = []
 
 REDIS_PERSISTENCE = {
     'HOST': '127.0.0.1',
