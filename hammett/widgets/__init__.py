c28ec0c12b6d022567517008fae5bdc464575b2b
Evgeny Golyshev <eugulixes@gmail.com>
2023-08-20 22:01:01 +0300

    Add choice widgets

---
+++ hammett/widgets/__init__.py
@@ -0,0 +1,12 @@
+"""
+The package contains a widget library for solving various
+common tasks in bot development.
+"""
+
+__all__ = (
+    'MultiChoiceWidget',
+    'SingleChoiceWidget',
+)
+
+from hammett.widgets.multi_choice_widget import MultiChoiceWidget
+from hammett.widgets.single_choice_widget import SingleChoiceWidget
---
+++ hammett/widgets/single_choice_widget.py
@@ -0,0 +1,42 @@
+"""The module contains the implementation of the single choice widget."""
+
+from contextlib import suppress
+from typing import TYPE_CHECKING
+
+from hammett.widgets.base import BaseChoiceWidget
+
+if TYPE_CHECKING:
+    from telegram import Update
+    from telegram.ext import CallbackContext
+    from telegram.ext._utils.types import BD, BT, CD, UD
+    from typing_extensions import Self
+
+    from hammett.widgets.types import WidgetState
+
+
+class SingleChoiceWidget(BaseChoiceWidget):
+    """The class implements the single choice widget."""
+
+    chosen_emoji = '🔘'
+    unchosen_emoji = '◯'
+
+    async def get_choice(
+        self: 'Self',
+        update: 'Update',
+        context: 'CallbackContext[BT, UD, CD, BD]',
+    ) -> 'WidgetState':
+        """Returns the choice made by the user."""
+
+        return await self.get_state(update, context)
+
+    async def switch(
+        self: 'Self',
+        choice: tuple[str, str],
+        choices: 'WidgetState',
+    ) -> None:
+        """Switches the widget from one state to another."""
+
+        with suppress(IndexError):
+            choices.pop()
+
+        choices.append(choice)
