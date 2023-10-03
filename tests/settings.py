d95f524379cd9917bf61bc82386aad4cd6874023
Polina Migel <p.ooooh@yandex.ru>
2023-10-03 18:34:58 +0300

    tests: test execution order of permissions

--- tests/settings.py
+++ tests/settings.py
@@ -4,4 +4,9 @@ IS_ADMIN = False
 
 IS_MODERATOR = False
 
+PERMISSIONS = [
+    'tests.test_permissions_mechanism.MainPermission',
+    'tests.test_permissions_mechanism.SubPermission',
+]
+
 TOKEN = 'secret-token'  # noqa: S105
