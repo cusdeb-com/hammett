da2450b3bdcefbdbd5801e0a5ae346b65d0bee07
Dmitriy Ivanko <tmwsls12@gmail.com>
2023-12-14 17:10:42 +0300

    Improve Screen interface adding jump method

--- hammett/widgets/carousel_widget.py
+++ hammett/widgets/carousel_widget.py
@@ -7,7 +7,6 @@ from hammett.core import Button
 from hammett.core.constants import DEFAULT_STATE, RenderConfig, SourcesTypes
 from hammett.core.exceptions import ImproperlyConfigured
 from hammett.core.handlers import register_button_handler
-from hammett.core.screen import NotificationScreen, StartScreen
 from hammett.widgets.base import BaseWidget
 from hammett.widgets.exceptions import FailedToGetStateKey, MissingPersistence
 
@@ -25,7 +24,7 @@ if TYPE_CHECKING:
 _END_POSITION, _START_POSITION = -1, 0
 
 
-class CarouselWidget(BaseWidget, NotificationScreen, StartScreen):
+class CarouselWidget(BaseWidget):
     """Implements the display of a carousel widget with control buttons
     for a list of images.
     """
@@ -348,7 +347,7 @@ class CarouselWidget(BaseWidget, NotificationScreen, StartScreen):
 
         return await self._init(update, context)
 
-    async def start(
+    async def jump(
         self: 'Self',
         update: 'Update',
         context: 'CallbackContext[BT, UD, CD, BD]',
