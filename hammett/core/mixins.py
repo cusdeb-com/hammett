5982cc2a7f0261781f8dc21c9cc2d3dd46390566
Dmitriy Ivanko <tmwsls12@gmail.com>
2024-02-12 16:03:47 +0300

    Move RouteMixin to mixins module

---
+++ hammett/core/mixins.py
@@ -0,0 +1,83 @@
+"""The module contains mixins."""
+
+from typing import TYPE_CHECKING
+
+from hammett.core import Screen
+from hammett.core.exceptions import ScreenRouteIsEmpty
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    from telegram import Update
+    from telegram.ext import CallbackContext
+    from telegram.ext._utils.types import BD, BT, CD, UD
+    from typing_extensions import Self
+
+    from hammett.types import Routes, State
+
+
+class RouteMixin(Screen):
+    """Mixin to switch between screens which are registered
+    in different states.
+    """
+
+    routes: 'Routes | None' = None
+
+    def __init__(self: 'Self') -> None:
+        super().__init__()
+
+        if self.routes is None:
+            msg = f'The route of {self.__class__.__name__} is empty'
+            raise ScreenRouteIsEmpty(msg)
+
+    async def _get_return_state_from_routes(
+        self: 'Self',
+        update: 'Update',
+        context: 'CallbackContext[BT, UD, CD, BD]',
+        routes: 'Routes',
+    ) -> 'State':
+        """Returns the first found state in the routes."""
+
+        current_state = await self.get_current_state(update, context)
+
+        for route in routes:
+            route_states, return_state = route
+            if current_state in route_states:
+                return return_state
+
+        return current_state
+
+    async def sgoto(
+        self: 'Self',
+        update: 'Update',
+        context: 'CallbackContext[BT, UD, CD, BD]',
+        **kwargs: 'Any',
+    ) -> 'State':
+        """Changes the state and switches to the screen re-rendering
+        the previous message.
+        """
+
+        config = await self.get_config(update, context, **kwargs)
+
+        await self.render(update, context, config=config)
+        return await self._get_return_state_from_routes(
+            update, context, self.routes,  # type: ignore[arg-type]
+        )
+
+    async def sjump(
+        self: 'Self',
+        update: 'Update',
+        context: 'CallbackContext[BT, UD, CD, BD]',
+        **kwargs: 'Any',
+    ) -> 'State':
+        """Changes the state and switches to the screen sending
+        it as a new message.
+        """
+
+        config = await self.get_config(update, context, **kwargs)
+        config.as_new_message = True
+
+        await self.render(update, context, config=config)
+        return await self._get_return_state_from_routes(
+            update, context, self.routes,  # type: ignore[arg-type]
+        )
