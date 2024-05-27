dd4e7646072e5e85ac886c2de701a9e055572f0f
Polina Migel <p.ooooh@yandex.ru>
2024-05-27 17:50:43 +0300

    Do not pass routes to _get_return_state_from_routes method

--- hammett/core/mixins.py
+++ hammett/core/mixins.py
@@ -34,13 +34,12 @@ class RouteMixin(Screen):
         self: 'Self',
         update: 'Update',
         context: 'CallbackContext[BT, UD, CD, BD]',
-        routes: 'Routes',
     ) -> 'State':
         """Returns the first found state in the routes."""
 
         current_state = await self.get_current_state(update, context)
 
-        for route in routes:
+        for route in self.routes:  # type: ignore[union-attr]
             route_states, return_state = route
             if current_state in route_states:
                 return return_state
@@ -60,9 +59,7 @@ class RouteMixin(Screen):
         config = await self.get_config(update, context, **kwargs)
 
         await self.render(update, context, config=config)
-        return await self._get_return_state_from_routes(
-            update, context, self.routes,  # type: ignore[arg-type]
-        )
+        return await self._get_return_state_from_routes(update, context)
 
     async def sjump(
         self: 'Self',
@@ -78,9 +75,7 @@ class RouteMixin(Screen):
         config.as_new_message = True
 
         await self.render(update, context, config=config)
-        return await self._get_return_state_from_routes(
-            update, context, self.routes,  # type: ignore[arg-type]
-        )
+        return await self._get_return_state_from_routes(update, context)
 
 
 class StartMixin(Screen):
