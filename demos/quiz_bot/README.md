ba41f6d7dad3e5887c4e997b51dbbd57d4fcd49e
Polina Migel <polinamigel@gmail.com>
2025-01-29 19:00:19 +0300

    demos: implement HammettQuizBot

---
+++ demos/quiz_bot/README.md
@@ -0,0 +1,73 @@
+# HammettQuizBot
+
+This demo showcases the `SingleChoiceWidget` and `MultiChoiceWidget` widgets to tackle various tasks. It demonstrates how they can be used to create a quiz with options for selecting one or multiple correct answers. Additionally, the bot illustrates how to use the `.po` files to create multilingual interfaces. Check out the documentation for more details.
+
+## Table of Contents:
+
+- [Installation](#installation)
+- [Run the Bot](#run-the-bot)
+- [Run the Tests](#run-the-tests)
+- [Docker](#docker)
+
+## Installation
+
+First, navigate to the `demos/quiz_bot/` directory:
+
+```bash
+$ cd demos/quiz_bot/
+```
+
+Next, create and activate a virtual environment:
+
+```bash
+$ virtualenv -p python3 quiz-bot-env
+$ source ./quiz-bot-env/bin/activate
+```
+
+Then, install the necessary dependencies from `requirements.txt`:
+
+```bash
+$ pip install -r requirements.txt
+```
+
+## Run the Bot
+
+Firstly, you need to generate .mo files for a translation support using the following command:
+
+```bash
+$ msgfmt demos/quiz_bot/locale/en/LC_MESSAGES/hammett.po -o demos/quiz_bot/locale/en/LC_MESSAGES/hammett.mo \
+&& msgfmt demos/quiz_bot/locale/pt-br/LC_MESSAGES/hammett.po -o demos/quiz_bot/locale/pt-br/LC_MESSAGES/hammett.mo \
+&& msgfmt demos/quiz_bot/locale/ru/LC_MESSAGES/hammett.po -o demos/quiz_bot/locale/ru/LC_MESSAGES/hammett.mo
+```
+
+Secondly, you need to set the `HAMMETT_SETTINGS_MODULE` and `TOKEN` environment variables. And only after that, run the `demo.py` script using the following command:
+
+```bash
+$ env HAMMETT_SETTINGS_MODULE=settings TOKEN=your-token python3 demo.py
+```
+
+Alternatively, you can create a `.env` file to specify the `TOKEN`. In this case, execute these commands:
+
+```bash
+$ source .env
+$ env HAMMETT_SETTINGS_MODULE=settings python3 demo.py
+```
+
+After completing these steps, the bot will be up and ready to accept the `/start` command.
+
+## Run the Tests
+
+The demo includes tests located in the `tests.py` module. To run the tests, use the following command:
+
+```bash
+$ env TOKEN=test-token python3 tests.py
+```
+
+## Docker
+
+To run the demo in a Docker container, first navigate to the `demos/` directory and execute the following commands:
+
+```bash
+$ docker compose build quiz-bot
+$ docker compose run quiz-bot
+```
---
+++ demos/quiz_bot/tests.py
@@ -0,0 +1,94 @@
+"""The module contains the tests for HammettQuizBot."""
+
+# ruff: noqa: I001
+
+import gettext
+import json
+import os
+import unittest
+from pathlib import Path
+
+from hammett.conf import settings
+from hammett.core import Button
+from hammett.core.constants import RenderConfig, SourceTypes
+from hammett.test.base import BaseTestCase
+from hammett.test.utils import catch_render_config
+from hammett.utils.translation import gettext as _
+
+from screens import LanguageSwitcherScreen, MainMenuScreen
+
+
+class HammettQuizBotTests(BaseTestCase):
+    """The class contains the tests for HammettQuizBot."""
+
+    def setUp(self):
+        """Initialize questions from the file for testing."""
+        gettext.textdomain(settings.DOMAIN)
+        gettext.bindtextdomain(settings.DOMAIN, settings.LOCALE_PATH)
+
+        with Path.open(settings.BASE_DIR / 'questions.json') as file:
+            self.questions = json.loads(file.read())
+
+    @catch_render_config()
+    async def test_en_main_menu_screen_render_after_calling_start_handler(self, actual):
+        """Test calling the `start` handler to get the final render config."""
+        await MainMenuScreen().start(self.update, self.context)
+
+        expected = self.prepare_final_render_config(RenderConfig(
+            as_new_message=True,
+            description=_(MainMenuScreen.caption),
+            hide_keyboard=True,
+            keyboard=[
+                [Button(_('❓ Start Quiz'), MainMenuScreen().start_quiz_handler,
+                    source_type=SourceTypes.HANDLER_SOURCE_TYPE)],
+                [Button(_('🌍 Language'), LanguageSwitcherScreen,
+                    source_type=SourceTypes.MOVE_SOURCE_TYPE)],
+            ],
+        ))
+        self.assertFinalRenderConfigEqual(expected, actual.final_render_config)
+
+    @catch_render_config()
+    async def test_pt_br_main_menu_screen_render_after_calling_start_handler(self, actual):
+        """Test calling the `start` handler to get the final render config."""
+        self.context.user_data['language_code'] = 'pt-br'
+
+        await MainMenuScreen().start(self.update, self.context)
+
+        expected = self.prepare_final_render_config(RenderConfig(
+            as_new_message=True,
+            description=_(MainMenuScreen.caption, 'pt-br'),
+            hide_keyboard=True,
+            keyboard=[
+                [Button(_('❓ Start Quiz', 'pt-br'), MainMenuScreen().start_quiz_handler,
+                    source_type=SourceTypes.HANDLER_SOURCE_TYPE)],
+                [Button(_('🌍 Language', 'pt-br'), LanguageSwitcherScreen,
+                    source_type=SourceTypes.MOVE_SOURCE_TYPE)],
+            ],
+        ))
+        self.assertFinalRenderConfigEqual(expected, actual.final_render_config)
+
+    @catch_render_config()
+    async def test_ru_main_menu_screen_render_after_calling_start_handler(self, actual):
+        """Test calling the `start` handler to get the final render config."""
+        self.context.user_data['language_code'] = 'ru'
+
+        await MainMenuScreen().start(self.update, self.context)
+
+        expected = self.prepare_final_render_config(RenderConfig(
+            as_new_message=True,
+            description=_(MainMenuScreen.caption, 'ru'),
+            hide_keyboard=True,
+            keyboard=[
+                [Button(_('❓ Start Quiz', 'ru'), MainMenuScreen().start_quiz_handler,
+                    source_type=SourceTypes.HANDLER_SOURCE_TYPE)],
+                [Button(_('🌍 Language', 'ru'), LanguageSwitcherScreen,
+                    source_type=SourceTypes.MOVE_SOURCE_TYPE)],
+            ],
+        ))
+        self.assertFinalRenderConfigEqual(expected, actual.final_render_config)
+
+
+if __name__ == '__main__':
+    os.environ.setdefault('HAMMETT_SETTINGS_MODULE', 'settings')
+
+    unittest.main()
