ce25ebe92c50689734c92b4452a09ddb29c0765d
Polina Migel <polinamigel@gmail.com>
2025-02-03 13:16:19 +0300

    demos: remove carousel in favor of HammettCarouselBot

--- demos/carousel/settings.py
+++
@@ -1,8 +0,0 @@
-"""The module contains the settings of the demo."""
-
-import os
-from pathlib import Path
-
-MEDIA_ROOT = Path(__file__).resolve().parent / 'media'
-
-TOKEN = os.getenv('TOKEN', '')
