85074a90cf521e633755a0d18bd879cf4d299d9c
Polina Migel <polinamigel@gmail.com>
2025-02-03 12:04:12 +0300

    demos: remove hiders in favor of HammettAdminPanelBot

--- demos/hiders/settings.py
+++
@@ -1,9 +0,0 @@
-"""The module contains the settings of the demo."""
-
-import os
-
-ADMIN_GROUP = []
-
-HIDERS_CHECKER = 'demos.hiders.hiders_checker.DemoHidersChecker'
-
-TOKEN = os.getenv('TOKEN', '')
