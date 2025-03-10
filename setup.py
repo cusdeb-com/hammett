58a758a915154cb44d5b35e2b237baeef1c1d5c7
Polina Migel <polinamigel@gmail.com>
2025-03-10 12:29:47 +0300

    Add info about Python 3.12 support to setup.py

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Communications :: Chat',
     ],
