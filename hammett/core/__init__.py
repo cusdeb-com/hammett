d9199bd00bba311240cb72582aded5449bb03225
Polina Migel <polinamigel@gmail.com>
2025-03-14 15:18:40 +0300

    Rename application.py to bot.py

--- hammett/core/__init__.py
+++ hammett/core/__init__.py
@@ -1,6 +1,6 @@
 """The core of the Hammett framework."""
 
-from hammett.core.application import Application
+from hammett.core.bot import Application
 from hammett.core.button import Button
 from hammett.core.screen import Screen
 
