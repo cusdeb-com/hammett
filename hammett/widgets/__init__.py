8c59c33448b681e5eb2edbeea47a05cd6a2adf3b
Polina Migel <polinamigel@gmail.com>
2025-05-05 18:24:24 +0300

    Implement CalendarWidget

--- hammett/widgets/__init__.py
+++ hammett/widgets/__init__.py
@@ -3,11 +3,13 @@ common tasks in bot development.
 """
 
 __all__ = (
+    'CalendarWidget',
     'CarouselWidget',
     'MultiChoiceWidget',
     'SingleChoiceWidget',
 )
 
+from hammett.widgets.calendar_widget import CalendarWidget
 from hammett.widgets.carousel_widget import CarouselWidget
 from hammett.widgets.multi_choice_widget import MultiChoiceWidget
 from hammett.widgets.single_choice_widget import SingleChoiceWidget
--- tests/run_tests.py
+++ tests/run_tests.py
@@ -15,6 +15,7 @@ from tests.test_permissions_mechanism import PermissionsTests
 from tests.test_persistence import PersistenceTests
 from tests.test_screens import ScreenTests
 from tests.test_start_marker import StartMarkerTests
+from tests.test_widgets.test_calendar import CalendarWidgetTests
 from tests.test_widgets.test_carousel import CarouselWidgetTests
 
 if __name__ == '__main__':
