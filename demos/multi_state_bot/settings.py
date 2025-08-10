2bcef445e3a0c3e8388dd5b9c053a3b6ca296faa
Polina Migel <p.ooooh@yandex.ru>
2025-08-10 17:08:56 +0300

    demos: enable debug logging in MultiStateBot

--- demos/multi_state_bot/settings.py
+++ demos/multi_state_bot/settings.py
@@ -7,6 +7,32 @@ with contextlib.suppress(ImportError):
     from dotenv import load_dotenv
     load_dotenv()
 
+LOGGING_LEVEL = os.getenv('LOGGING_LEVEL', 'DEBUG')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '{levelname}: {name}: {asctime}: {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'formatter': 'standard',
+            'level': LOGGING_LEVEL,
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'hammett': {
+            'handlers': ['console'],
+            'level': LOGGING_LEVEL,
+        },
+    },
+}
+
 REDIS_PERSISTENCE = {
     'HOST': os.getenv('REDIS_PERSISTENCE_HOST', 'valkey'),
     'PORT': 6379,
