f2c73cd8ff3264ae256d226a6ef30ff39ae67cd6
Polina Migel <polinamigel@gmail.com>
2025-10-03 15:04:20 +0300

    tests: add missing tests for MultiChoiceWidget

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -29,6 +29,7 @@ from tests.utils.test_translation import (
 )
 from tests.widgets.test_calendar import CalendarWidgetTests
 from tests.widgets.test_carousel import CarouselWidgetTests, CarouselWidgetWithoutUpdateTests
+from tests.widgets.test_multi_choice import MultiChoiceWidgetTests
 from tests.widgets.test_single_choice import SingleChoiceWidgetTests
 
 if __name__ == '__main__':
