d3c10c846b40e3497a2a3ed606d55186374a659e
Polina Migel <polinamigel@gmail.com>
2026-01-20 12:27:26 +0300

    tests: implement BaseStateWidgetTestsWithoutUpdate

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -34,7 +34,12 @@ from tests.utils.test_translation import (
     UtilsTranslationCatalogTests,
     UtilsTranslationTests,
 )
-from tests.widgets.test_base import BaseChoiceWidgetTests, BaseStateWidgetTests, BaseWidgetTests
+from tests.widgets.test_base import (
+    BaseChoiceWidgetTests,
+    BaseStateWidgetTests,
+    BaseStateWidgetTestsWithoutUpdate,
+    BaseWidgetTests,
+)
 from tests.widgets.test_calendar import CalendarWidgetTests
 from tests.widgets.test_carousel import CarouselWidgetTests, CarouselWidgetWithoutUpdateTests
 from tests.widgets.test_multi_choice import (
