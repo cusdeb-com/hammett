448e5365fc9127a5e3e0ad66ab5efbcdee292fe4
Polina Migel <polinamigel@gmail.com>
2026-02-17 16:49:54 +0300

    demos: set 127.0.0.1 as default value for REDIS_PERSISTENCE_HOST

--- demos/say_hello_bot/settings.py
+++ demos/say_hello_bot/settings.py
@@ -9,7 +9,7 @@ with contextlib.suppress(ImportError):
     load_dotenv()
 
 REDIS_PERSISTENCE = {
-    'HOST': os.getenv('REDIS_PERSISTENCE_HOST', 'valkey'),
+    'HOST': os.getenv('REDIS_PERSISTENCE_HOST', '127.0.0.1'),
     'PORT': 6379,
     'DB': os.getenv('REDIS_PERSISTENCE_DB', '1'),
 }
